ad33f586918de539c850771d1bf0896155ba478b
leonsandoval <leon.sandoval@alumnos.duoc.cl>
2018-10-16 12:28:17 -0300

    cambios a formulario y modelo

--- MisPerris/core/admin.py
+++ MisPerris/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class ComunaAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre')
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre')
+
+
+
+
+
+admin.site.register(Region,RegionAdmin)
+admin.site.register(Comuna,ComunaAdmin)
