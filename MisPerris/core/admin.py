6f09898884a35b1f2a6132377c9d533fa377c3c4
Leonardo Sandoval Contreras <familiasc26@hotmail.com>
2018-10-23 21:02:09 -0300

    cambios menores

--- MisPerris/core/admin.py
+++ MisPerris/core/admin.py
@@ -15,11 +15,12 @@ class RazaAdmin(admin.ModelAdmin):
 class EstadoAdmin(admin.ModelAdmin):
     list_display = ('id','nombre')
 
-
+class MascotaAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre','raza','genero','fechaIngreso','fechaNacimiento','descripcion','foto','estado')
 
 admin.site.register(Region,RegionAdmin)
 admin.site.register(Comuna,ComunaAdmin)
 admin.site.register(Estado,EstadoAdmin)
 admin.site.register(Raza,RazaAdmin)
-admin.site.register(Mascota)
+admin.site.register(Mascota, MascotaAdmin)
 admin.site.register(Persona)
